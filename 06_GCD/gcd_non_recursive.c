6a59d4598e16b68b41921f1b5d2d00d44cff9f63
ArpitBreathes <proton.arpitpandey@gmail.com>
2025-06-04 22:04:24 +0530

    Updated GCD non-recursive program

--- 06_GCD/gcd_non_recursive.c
+++ 06_GCD/gcd_non_recursive.c
@@ -1,11 +1,13 @@
 #include<conio.h>
 #include<stdio.h>
 
-main(){
+void main(){
     int num1,num2;
     clrscr();
     printf("Enter two positive integers: ");
     scanf("%d %d", &num1, &num2);
+    if (num1 < 0) num1 = -num1;
+    if (num2 < 0) num2 = -num2;
     while(num2!=0){
         int temp = num2;;
         num2 = num1%num2;
