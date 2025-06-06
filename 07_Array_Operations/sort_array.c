1c8931582609a84fd5936dd729a72342cfaaba18
ArpitBreathes <proton.arpitpandey@gmail.com>
2025-06-06 22:54:11 +0530

    C program to Sort the Array in Ascending order

---
+++ 07_Array_Operations/sort_array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap the elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void main() {
+    int arr[100], n, i;
+    
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    getch();
+}
